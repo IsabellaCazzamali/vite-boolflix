b2131135a29fcaa28ff859c94723d9882b710493
IsabellaCazzamali <isa.cazzamali@gmail.com>
2024-04-03 19:11:29 +0200

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,72 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+export default {
+  name: 'App',
+  data(){
+    return {
+      movies: [], 
+      image: 'https://api.themoviedb.org/3/configuration&',
+      searchMovie: ''
+    }
+  },
+  components: {
+    AppHeader
+  },
+  mounted() {
+    axios.get ('https://api.themoviedb.org/3/discover/movie', {
+      headers: {
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTEyODZmYzgwYjQzMGVjMDBjMDkzMDQzYTc2ODlkOCIsInN1YiI6IjY2MDcyOTNkOGEwZTliMDE3YzRkYWFjMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-9d58jzGjZ-xwHoqGheDr2mBup6cqJAyjQK3srHGmvk'
+      }
+    })
+    .then(response => {
+      console.log(response.data);
+      console.log(response.data.results);
+      console.log(this);
+      this.movies = response.data.results;
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+}
 </script>
 
 <template>
+
+  <AppHeader></AppHeader>
+
+<div class="search_bar">
+  <input type="text" name="searchMovie" placeholder="Type the name of a movie to search" v-model="searchMovie">
+
+  <button @click="$emit('searched', [searchMovie])">Search</button>
+</div>
+
+
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="movie" v-for="movie in movies">
+      <img :src="movie.image.poster_path" alt="movie poster">
+      <h3>{{ movie.title }}</h3>
+      <h4>{{ movie.original_title }}</h4>
+      <h5>{{ movie.original_language }}</h5>
+      <h5>{{ movie.vote_average }}</h5>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+
+.search_bar{
+  margin-top: 1rem;
+  margin-left: 1rem;
+  &
+  input, button {
+    padding: 0.5rem;
+    margin: 0.3rem;
+    border-radius: 0.25rem;
+    border-style: none;
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'AppHeader'
+  }
+</script>
+
+<template>
+<h1>Boolflix</h1>
+</template>
+
+<style>
+h1{
+  color: red;
+  text-align: center;
+  margin-top: 1rem;
+}
+</style>
